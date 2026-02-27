a45d2270e81a311ee9adbcde861008fefcb3f2cb
mokdad nadjib <nadji1526@hotmail.com>
2026-02-27 09:43:11 +0100

    Contact: confirmation après envoi

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,17 +10,29 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
     public function index(Request $request): Response
     {
+        // Création du formulaire
         $form = $this->createForm(ContactType::class);
 
+        // Récupère les données envoyées (POST)
         $form->handleRequest($request);
 
+        // Si l’utilisateur a cliqué sur "Envoyer" et que tout est valide
         if ($form->isSubmitted() && $form->isValid()) {
-            // Ici plus tard : envoyer email ou enregistrer en base
+
+            // (Optionnel) récupère les données du formulaire
+            $data = $form->getData();
+
+            // Message de confirmation
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+
+            // Redirection (évite le renvoi du formulaire au refresh)
+            return $this->redirectToRoute('app_contact');
         }
 
+        // Affichage de la page
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(),
         ]);
